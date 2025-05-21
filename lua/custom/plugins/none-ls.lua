4b724c00f05dcc69bee8c2d913d4109cd92cc576
zubrzubr <maxzub@gmail.com>
2025-05-21 16:13:05 +0200

    Disable autofromat, create command leader fm

--- lua/custom/plugins/none-ls.lua
+++ lua/custom/plugins/none-ls.lua
@@ -41,14 +41,14 @@ return {
       -- you can reuse a shared lspconfig on_attach callback here
       on_attach = function(client, bufnr)
         if client.supports_method 'textDocument/formatting' then
-          vim.api.nvim_clear_autocmds { group = augroup, buffer = bufnr }
-          vim.api.nvim_create_autocmd('BufWritePre', {
-            group = augroup,
-            buffer = bufnr,
-            callback = function()
-              vim.lsp.buf.format { async = false }
-            end,
-          })
+          -- vim.api.nvim_clear_autocmds { group = augroup, buffer = bufnr }
+          -- vim.api.nvim_create_autocmd('BufWritePre', {
+          -- group = augroup,
+          -- buffer = bufnr,
+          -- callback = function()
+          -- vim.lsp.buf.format { async = false }
+          -- end,
+          -- })
         end
       end,
     }
