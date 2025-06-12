39bbc649457798e06f229b98a6fa9f51a58b4747
zubrzubr <maxzub@gmail.com>
2025-06-12 16:29:55 +0200

    Enable pyright and ruff

--- init.lua
+++ init.lua
@@ -673,7 +673,8 @@ require('lazy').setup({
       local servers = {
         -- clangd = {},
         -- gopls = {},
-        -- pyright = {},
+        pyright = {},
+        ruff = {},
         -- rust_analyzer = {},
         -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
         --
